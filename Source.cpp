2f5221dd0945d16db65def72b28e8758d67bcf48
NickRimerQ <58778313+NickRimerQ@users.noreply.github.com>
2020-11-30 21:26:42 +0300

    Last cpp(main)

--- Source.cpp
+++ Source.cpp
@@ -11,9 +11,13 @@ int main()
 	}
 	art.push_front(33);
 	art.pop_back();
+	art.insert(22, 3);
 	for (int i = 0; i < art.GetSize(); i++)
 	{
 		cout << art[i] << endl;
 	}
+	cout << "-------------" << endl;
+	cout << art.isEmpty() << endl;
 	return 0;
+	
 }
